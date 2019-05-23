864ef96d15d8e49c863038c3103970687f7a4be6
Trivikram Kamat <16024985+trivikr@users.noreply.github.com>
2019-05-23 14:07:13 -0700

    require server after setting process.env variable

--- src/__tests__/server.test.js
+++ src/__tests__/server.test.js
@@ -1,10 +1,10 @@
 /**
  * @jest-environment node
  */
-const server = require("../server");
 const axios = require("axios");
 
 describe("server", () => {
+  let server;
   let httpsServer;
   const PORT = 2397;
   const rejectUnauthorizedEnv = process.env.NODE_TLS_REJECT_UNAUTHORIZED;
@@ -12,6 +12,7 @@ describe("server", () => {
   beforeEach(() => {
     // create server on port
     process.env.NODE_TLS_REJECT_UNAUTHORIZED = "0";
+    server = require("../server");
     httpsServer = server(PORT);
   });
 
